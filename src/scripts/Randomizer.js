a46ea5068b56559ec4f37ceda5d0b10104708866
ColtMay <colten_mayberry@pm.me>
2021-03-24 07:42:50 -0500

    working on do-while loop, not fully functional

--- src/scripts/Randomizer.js
+++
@@ -1,9 +0,0 @@
-import { getPhrases } from "./DataManager.js"
-
-// Function to randomize number based on array length.
-export const randomPhrase = () => {
-    getPhrases()
-        .then(phraseArray => {
-            return(phraseArray.length)
-        })
-}
