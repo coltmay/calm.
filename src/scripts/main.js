9631e1f28ad9fbe66d5274bc6b449e7d5fba6569
ColtMay <colten_mayberry@pm.me>
2021-03-24 08:12:41 -0500

    fixed repetition preventor

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,28 +8,35 @@ const buttonElement = document.querySelector(".buttonFocus")
 
 // ! Look into `Fisher-Yates Algorithm`
 
+
 let lastRandom = 0;
 
 const ShowRandomPhrase = () => {
     getPhrases()
         .then(() => {
+            // Set variable equal to the length of the array being fetched.
             let length = phraseLength();
-
+            // Set variable equal to a random number between 0 and the array length.
             let randomIndex = randomSelector(length);
-            lastRandom = randomIndex;
-            console.log(lastRandom);
-            let randomPhrase = usePhrases()[randomIndex]
 
+
+            while (lastRandom === randomIndex) {
+                randomIndex = randomSelector(length)
+            }
+            console.log(`Random Index = ${randomIndex}`, `Last Random = ${lastRandom}`)
+            
+            let randomPhrase = usePhrases()[randomIndex]
+            
             let phrase = DisplayPhrase(randomPhrase)
             
             phraseElement.innerHTML = phrase;
+
+            lastRandom = randomIndex;
         })
 }
 
 const randomSelector = (length) => {
-    do {
     return Math.floor((Math.random() * length));
-    } while (lastRandom === randomIndex);
 }
 
 const ShowRandomButton = () => {
